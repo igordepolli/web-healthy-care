47f45bccfdf452ab06f759bb29c7d8c433e71064
Igor Depolli <igorcler@gmail.com>
2023-04-05 18:28:36 -0300

    Login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "", path_names: { sign_in: "login", sign_up: "registrar", sign_out: "logout", password: "senha" }
 
   resources :patients, only: [:new, :show, :create]
 
