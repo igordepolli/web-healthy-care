a86ef693c4998af9392b99d60bacb728c3944acc
Igor Depolli <igorcler@gmail.com>
2023-04-05 19:01:38 -0300

    Logout setup

--- app/helpers/design_system/fudgeball/components_helper.rb
+++ app/helpers/design_system/fudgeball/components_helper.rb
@@ -9,4 +9,11 @@ module DesignSystem::Fudgeball::ComponentsHelper
 
     link_to text, url, class: "bg-#{color}-500 hover:bg-#{color}-700 text-#{text_color} font-bold py-2 px-4 rounded #{classes}", **html_options
   end
+
+  def navbar_button(text, options = {}, html_options = {})
+    url     = options.delete(:url)
+    classes = html_options.delete(:class)
+
+    link_to text, url, class: "inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 #{classes}", **html_options
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, path: "", path_names: { sign_in: "login", sign_up: "registrar", sign_out: "logout", password: "senha" }
+  scope(path_names: { new: "novo", edit: "editar" }) do
+    devise_for :users, path: "usuarios", path_names: { sign_in: "login", sign_up: "registrar", sign_out: "logout", password: "senha" }
 
-  resources :patients, only: [:new, :show, :create]
+    resources :patients, only: [:new, :show, :create], path: "pacientes"
 
-  root to: "home#show"
+    root to: "home#show"
+  end
 end
