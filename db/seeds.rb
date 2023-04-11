bfd1cc502d21dcccb49b3e718ea6959aeabebce4
Igor Depolli <igorcler@gmail.com>
2023-04-10 21:28:03 -0300

    New seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@ User.insert({
   encrypted_password: User.new.send(:password_digest, "123456"),
   classification: :admin
 })
+
+leo    = User.create! email: "leo@gmail.com",    name: "Leonardo", last_name: "Maralha", password: "123456", classification: :patient
+milena = User.create! email: "milena@gmail.com", name: "Milena",   last_name: "Regiani", password: "123456", classification: :doctor
+oreo   = User.create! email: "oreo@gmail.com",   name: "Oreo",     last_name: "Depolli", password: "123456", classification: :patient
+
+Patient.create! user: leo,    name: "Leonardo", last_name: "Maralha"
+Patient.create! user: oreo,   name: "Oreo",     last_name: "Depolli"
+Doctor.create!  user: milena, name: "Milena",   last_name: "Regiani", cpf: "153.316.417-76", crm: "CRM/SP 123456"
