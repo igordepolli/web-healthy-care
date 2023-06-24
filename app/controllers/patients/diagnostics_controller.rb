74c7c284c291ac33c38411331e2e3e10a09275e9
Igor Depolli <igorcler@gmail.com>
2023-06-23 21:57:07 -0300

    Begin responsive treatment

--- app/controllers/patients/diagnostics_controller.rb
+++ app/controllers/patients/diagnostics_controller.rb
@@ -32,7 +32,8 @@ class Patients::DiagnosticsController < Patients::DashboardsController
     @diagnostic    = @patient.diagnostics.new disease:, **treated_params
 
     if @diagnostic.save
-      redirect_to patient_diagnostic_path(@patient, @diagnostic)
+      # redirect_to patient_diagnostic_path(@patient, @diagnostic)
+      redirect_to new_patient_diagnostic_treatment_path(@patient, @diagnostic)
     else
       flash[:error] = @diagnostic.errors.full_messages
       render :new, status: :unprocessable_entity
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def mobile?
+    user_agent = request.user_agent.downcase
+    user_agent.include?("mobile") || user_agent.include?("android") || user_agent.include?("iphone")
+  end
+
+  def desktop?
+    !mobile?
+  end
 end
